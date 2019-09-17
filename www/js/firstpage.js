0e09b9e062c11c62326695e95ebc141daa54a6f4
Imran Mulani <aarfaatechnovision@gmail.com>
2019-09-17 17:24:27 +0530

    Home Page

---
+++ www/js/firstpage.js
@@ -0,0 +1,13 @@
+$('#datepicker').datepicker({
+    uiLibrary: 'bootstrap4'
+});
+
+$('#timepicker').timepicker();
+                            
+$('#datepicker1').datepicker({
+    uiLibrary: 'bootstrap4'
+});
+
+$('#timepicker1').timepicker({
+    autoclose: true
+});
